c23a7f34d5054ea087af4648b6fc2236a5c1dc57
Mike Purvis <mikedotexe@gmail.com>
2020-10-09 14:01:05 -0700

    minor cleanup

--- index.js
+++ index.js
@@ -66,8 +66,7 @@ const getNear = async () => {
 const createNearAccountObj = async () => {
   const n = await getNear();
   if (n) {
-    const acct = await n.account(NEAR_LOCAL_ACCOUNT_ID);
-    return acct;
+    return await n.account(NEAR_LOCAL_ACCOUNT_ID);
   } else {
     console.error('Could not get NEAR object'); // need this?
   }
@@ -113,22 +112,22 @@ app.post('/', async function(req, res){
   console.log('recovered Ethereum address', recoveredAddress);
 
   const account = await getNearAccount();
-  console.log(`Current NEAR account ${account} becomes…`);
+  console.log(`Current NEAR account ${account.accountId} becomes…`);
   const accountEvmAddress = utils.nearAccountToEvmAddress(account.accountId);
   console.log('accountEvmAddress', accountEvmAddress);
 
-  console.log('Getting nonce for that account…');
   // Why doesn't this work?
+  // console.log('Getting nonce for that account…');
 
   // hardcoded for counter.test.near
   // we can try doing super simple calls/view like "increment" and "get_num" respectively
-  const counterEvmAddress = '0xe45c4034a989e1a4ce9207e312f71e783e019eb9';
-  const counterAddressArg = Buffer.from(counterEvmAddress.substr(2), 'hex');
+  // const counterEvmAddress = '0xe45c4034a989e1a4ce9207e312f71e783e019eb9';
+  // const counterAddressArg = Buffer.from(counterEvmAddress.substr(2), 'hex');
 
   // remove the '0x' and hex encode
   // account's evm address turned into expected 20 bytes
   // See "AddressArg" in nearcore/runtime/near-evm-runner/src/lib.rs
-  const addressArg = Buffer.from(accountEvmAddress.substr(2), 'hex');
+  // const addressArg = Buffer.from(accountEvmAddress.substr(2), 'hex');
 
   // In another Rust project Borsh serialized this:
   /*
@@ -142,19 +141,19 @@ app.post('/', async function(req, res){
   // where the arrays are a mixture of using Node repl and dtool's h2a
   // I honestly think we may need to include Borsh macros in the nearcore repo?
 
-  try {
-    let nonceResult = await account.viewFunction(
-      NEAR_LOCAL_EVM,
-      'get_nonce',
-      {
-        args: [60, 58, 79, 182, 43, 188, 16, 177, 146, 235, 112, 195, 121, 111, 0, 161, 24, 150, 86, 28, 60, 58, 79, 182, 43, 188, 16, 177, 146, 235, 112, 195, 121, 111, 0, 161, 24, 150, 86, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-      },
-    );
-    console.log('nonceResult', nonceResult);
-  } catch (e) {
-    console.error(e.message);
-    throw e;
-  }
+  // try {
+  //   let nonceResult = await account.viewFunction(
+  //     NEAR_LOCAL_EVM,
+  //     'get_nonce',
+  //     {
+  //       args: [228, 92, 64, 52, 169, 137, 225, 164, 206, 146, 7, 227, 18, 247, 30, 120, 62, 1, 158, 185]
+  //     },
+  //   );
+  //   console.log('nonceResult', nonceResult);
+  // } catch (e) {
+  //   console.error(e.message);
+  //   throw e;
+  // }
 
   // Figure out RLP encoded thing or whatever is needed, bytes-wise
   // in nearcore/runtime/near-evm-runner/src/lib.rs ("evm-precompile" branch)
